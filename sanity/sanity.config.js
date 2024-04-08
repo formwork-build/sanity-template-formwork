46ddbff46fc9848aa322ffd0969d99a3ea427983
Jack Fowler <jack@formwork.build>
2024-04-08 10:09:47 +1000

    handle singletons

--- sanity/sanity.config.js
+++ sanity/sanity.config.js
@@ -6,9 +6,14 @@ import { structure } from './deskStructure'
 import defaultDocumentNode from './defaultDocumentNode'
 import { noteField } from 'sanity-plugin-note-field'
 
+
 export const projectId = '20y199p2'
 export const dataset = 'production'
 export const vercelUrl = 'https://formwork-next.vercel.app'
+
+const singletonActions = new Set(["publish", "discardChanges", "restore"])
+
+const singletonTypes = new Set(["homePage", "siteSettings"])
 
 export default defineConfig({
 	name: 'default',
@@ -24,6 +29,14 @@ export default defineConfig({
 		noteField(),
 	],
 	schema: {
-		types: schemaTypes,
-	},
+    types: schemaTypes,
+    templates: (templates) =>
+      templates.filter(({ schemaType }) => !singletonTypes.has(schemaType)),
+  },
+	document: {
+    actions: (input, context) =>
+      singletonTypes.has(context.schemaType)
+        ? input.filter(({ action }) => action && singletonActions.has(action))
+        : input,
+  },
 })
